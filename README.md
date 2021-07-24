5ef17ebe91df6aadd89a10e27db12768291c2581
Alex Oloo <alekcz@gmail.com>
2021-07-24 18:08:13 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+## My website
+
+This is my website nothing fancy. 
+
+## Prerequisites
+You will need [PCP](https://github.com/alekcz/pcp) to run this project. 
+
+You can start your dev server like so
+
+``` shellsession
+$ pcp -s /path/to/server/root
+```
+## Deploying
+
+You can find instructions on deploying your PCP site with nginx [here](https://github.com/alekcz/pcp/blob/master/docs/replacing-php.md)
+
+## License
+
+Copyright © 2021 Alexander Oloo
+
+<img src="https://raw.githubusercontent.com/alekcz/pcp/master/assets/logo/logo-alt.svg" width="40px">
---
+++ public/index.clj
@@ -0,0 +1,47 @@
+(ns index
+  (:require [pcp :as pcp]
+            [garden.core :refer [css]]))
+
+(defn resp []
+  (pcp/html
+    [:html {:style "font-family: 'Source Sans Pro', Arial, Helvetica, sans-serif;text-align: center;"
+            :lang "en"}
+      [:head 
+        [:title "I'm Alex and I write code."]
+        [:link {:rel "shortcut icon" :type "image/png" :href "/favi.png"}]
+        [:meta {:charset "utf-8"}]
+        [:meta {:name "viewport" :content "width=device-width,initial-scale=1"}]
+        [:meta {:http-equiv "X-UA-Compatible" :content "IE=edge"}]
+        [:meta {:name "description" :content "I'm Alex and I write code."}]
+        [:meta {:name "keywords" :content "clojure, pcp, vue, design, web development, lisp"}]
+        [:meta {:name "author" :content "alekcz"}]
+        [:meta {:name "twitter:site" :content "https://alekcz.org"}]
+        [:meta {:name "twitter:card" :content "summary"}]
+        [:meta {:name "twitter:title" :content "I'm Alex and I write code."}]
+        [:meta {:name "twitter:description" :content "I'm Alex and I write code."}]
+        [:meta {:name "twitter:creator" :content "@alekcz"}]
+        [:meta {:name "twitter:image" :content "https://alekcz.org/alexander-round.png"}]
+        [:meta {:property "og:title" :content "I'm Alex and I write code."}]
+        [:meta {:property "og:type" :content "website"}]
+        [:meta {:property "og:url" :content "https://alekcz.org"}]
+        [:meta {:property "og:image" :content "https://alekcz.org/alexander-round.png"}]
+        [:meta {:property "og:site_name" :content "I'm Alex and I write code."}]
+        [:style 
+          (css [:p.info { :background-color "#EEE" 
+                          :font-size "13px"
+                          :margin-top "40px"
+                          :padding "10px" 
+                          :text-align "left" 
+                          :width "300px"}])
+          (css [:strong { :font-size "13px"}])
+          (css [:code   { :font-size "12px" 
+                          :font-weight "normal"}])]]
+      [:body 
+        [:div 
+              {:style "display: flex; flex-direction: column; justify-content: center; align-items: center; min-height: 90vh;"
+              }
+              [:img {:src "/alexander-round.png" :width "200px" :height "200px"}]
+              [:h1 "I write code."]
+              [:p "Catch me on GitHub: " [:a {:href "https://github.com/alekcz"} "@alekcz"]]]]]))
+
+(pcp/response 200 (resp) "text/html")            
